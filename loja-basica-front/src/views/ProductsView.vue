b3ad87374ea7f76d4349cfdb0f954f0926869300
kadu91878 <kadu_91878@hotmail.com>
2023-07-03 19:04:20 -0300

    Modificação no ProductsView agora com Filtro funcionando

--- loja-basica-front/src/views/ProductsView.vue
+++ loja-basica-front/src/views/ProductsView.vue
@@ -1,47 +1,58 @@
 <template>
   <NSpin :show="loading">
-    <n-card>
-      <template v-for="product in productList">
-        <ProductCard :category="product.category" :description="product.description" :title="product.title"
-        :image="product.image" :price="product.price"
-        />
-      </template>
-    </n-card>
+    <input type="text" v-model="keyword" placeholder="Enter keyword">
+    <button @click="filterProducts(keyword)">Filter</button>
+
+    <div v-for="product in filteredProducts" :key="product.id">
+      <ProductCard
+        :category="product.category"
+        :description="product.description"
+        :title="product.title"
+        :image="product.image"
+        :price="product.price"
+      />
+    </div>
   </NSpin>
 </template>
 
-
 <script setup lang="ts">
-import ProductCard from '../components/ProductCard.vue'
-import axios from "axios";
-import { onMounted, ref } from "vue";
-import type { product } from "../types/product";
-import { NSpin, NCard } from "naive-ui";
+import ProductCard from '../components/ProductCard.vue';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import type { product } from '../types/product';
+import { NSpin } from 'naive-ui';
 
-const productList = ref<product[]>([]);
-
+let productList = ref<product[]>([]);
 const loading = ref(false);
+let filteredProducts = ref<product[]>([]);
 
 const getProduct = async () => {
   try {
-    const response = await axios.get("https://fakestoreapi.com/products");
+    const response = await axios.get('https://fakestoreapi.com/products');
     productList.value = response.data;
-    console.log("produtos", productList.value);
+    filteredProducts.value = productList.value;
+    console.log('produtos', productList.value);
   } catch (error) {
     console.error(error);
   }
 };
 
-(async () => {
+onMounted(async () => {
   loading.value = true;
   await getProduct();
   loading.value = false;
-})();
-// async function getProductList() {
-//   loading.value = true;
-//   await axios.get("https://fakestoreapi.com/products").then((response) => {
-//     productList.value = response.data;
-//     loading.value = false;
-//   });
-// }
+});
+
+function filterProducts(keyword) {
+  const filtered = productList.value.filter((product) => {
+    for (const key in product) {
+      const value = product[key];
+      if (value.toString().includes(keyword)) {
+        return true;
+      }
+    }
+    return false;
+  });
+  filteredProducts.value = filtered;
+}
 </script>
